1ce7a7e62288a0434528a70904d5ef87b224f9ba
Isabele Akemi <70545832+isabeleakemi@users.noreply.github.com>
2023-02-13 03:05:57 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Cliente.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+
+        }
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+
+        public static List<Cliente> Todos()
+        {
+            var lista = new List<Cliente>();
+
+            SqlConnection conn = new SqlConnection(Conexao.Dados);
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand("select * from clientes", conn);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                lista.Add(new Cliente
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Nome = reader["Nome"].ToString(),
+                    Telefone = reader["Telefone"].ToString(),
+                    Endereco = reader["Endereco"].ToString()
+                });
+            }
+
+            return lista;
+        }
+    }
+}
